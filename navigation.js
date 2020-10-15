ab2f898982cfe28f2145012d9e431a5e23646b48
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2020-10-15 13:03:45 +0200

    Bug fix voor mobile menu

--- navigation.js
+++ navigation.js
@@ -1,12 +1,24 @@
-function mobileNavFunction() {
 
-    var nav = document.getElementById("mobile-nav");
-    var languageSelector = document.getElementById("mobile-nav-selector");
-    if (nav.style.display === "block") {
-            nav.style.display = "none";
-            languageSelector.style.display = "none";
-        } else {
+// global var declarations
+var nav = document.getElementById("mobile-nav");
+var languageSelector = document.getElementById("mobile-nav-selector");
+
+// basiswaarde van display none voor mobiel menu
+nav.style.display = 'none';
+
+// functie voor mobile navigatie openen/sluiten
+function mobileNavFunction() {
+    if (nav.style.display === "none") {
             nav.style.display = "block";
             languageSelector.style.display = "inline-flex";
+        } else {
+            nav.style.display = "none";
+            languageSelector.style.display = "none";
         }
 }
+
+// zorgt er voor dat menu sluit als je van desktop naar mobiel gaat
+window.addEventListener('resize', function() {
+  nav.style.display = 'none';
+  languageSelector.style.display = 'none';
+});
