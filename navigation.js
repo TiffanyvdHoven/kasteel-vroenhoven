8800a75c0f1bc76e08d7a2bd5ed3f61968be6ccd
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2020-10-15 12:44:40 +0200

    Fixed mobile menu
    probleem met niet sluiten mobile menu als je op een link klikt weggehaald (animatie ook weggehaald)

--- navigation.js
+++ navigation.js
@@ -1,14 +1,12 @@
-function mobileNavFunction(x) {
-
-  x.classList.toggle("change");
+function mobileNavFunction() {
 
     var nav = document.getElementById("mobile-nav");
     var languageSelector = document.getElementById("mobile-nav-selector");
     if (nav.style.display === "block") {
-        nav.style.display = "none";
-        languageSelector.style.display = "none";
-    } else {
-        nav.style.display = "block";
-        languageSelector.style.display = "inline-flex";
-    }
+            nav.style.display = "none";
+            languageSelector.style.display = "none";
+        } else {
+            nav.style.display = "block";
+            languageSelector.style.display = "inline-flex";
+        }
 }
