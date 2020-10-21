7a2b64054bd1de5a0453a82e47a2990884677bb0
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2020-10-21 11:26:49 +0200

    Slider implementatie
    Volledig responsive gemaakte slider met splide.js (alleen nl versie)

---
+++ slideshow.js
@@ -0,0 +1,43 @@
+
+
+
+    	new Splide( '.slider1', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+        interval   : 2000,
+    	} ).mount();
+
+      new Splide( '.slider2', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+    	} ).mount();
+
+      new Splide( '.slider3', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+        interval   : 2000,
+    	} ).mount();
+
+      new Splide( '.slider4', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+        interval   : 2000,
+    	} ).mount();
+
+      new Splide( '.slider5', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+        interval   : 2000,
+    	} ).mount();
+
+      new Splide( '.slider6', {
+        type      : 'loop',
+    		perPage    : 1,
+        autoplay   : true,
+        interval   : 2000,
+    	} ).mount();
