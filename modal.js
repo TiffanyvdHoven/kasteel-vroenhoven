9246193da2ecef4281879fd08bf7e41e6a9fe882
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2020-10-22 12:39:37 +0200

    Begin floorplan pop up

---
+++ modal.js
@@ -0,0 +1,47 @@
+var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("myBtn1");
+
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function openModal1() {
+  modal1.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function closeModal1() {
+  modal1.style.display = "none";
+}
+
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
