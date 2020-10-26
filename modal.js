90381b598093d419adbc497c2287cffa0d236a12
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2020-10-26 10:36:13 +0100

    Floorplan pop-up
    Floorplan pop-up werkt code moet nog DRY gemaakt worden

--- modal.js
+++ modal.js
@@ -1,24 +1,12 @@
 var modal = document.getElementById("myModal");
 var modal1 = document.getElementById("myModal1");
-
+var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
 
-
-// Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
 
 function openModal() {
   modal.style.display = "block";
@@ -26,6 +14,10 @@ function openModal() {
 
 function openModal1() {
   modal1.style.display = "block";
+}
+
+function openModal2() {
+  modal2.style.display = "block";
 }
 
 function closeModal() {
@@ -36,12 +28,18 @@ function closeModal1() {
   modal1.style.display = "none";
 }
 
-
+function closeModal2() {
+  modal2.style.display = "none";
+}
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  } else if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
